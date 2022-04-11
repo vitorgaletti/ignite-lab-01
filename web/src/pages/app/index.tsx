8d8719eff1a86922695624f96aaed6d5c8db210a
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-04-11 18:21:58 -0300

    :construction: Integrando front-end e back-end

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,14 +1,36 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import {
+  useGetProductsQuery,
+  useMeQuery
+} from '../../graphql/generated/graphql';
+import {
+  getServerPageGetProducts,
+  ssrGetProducts
+} from '../../graphql/generated/page';
+import { withApollo } from '../../lib/withApollo';
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser();
-  return (
-    <div>
-      <h1>Hello World</h1>
+  const { data: me } = useMeQuery();
 
+  return (
+    <div className="text-violet-500">
+      <h1>Hello World</h1>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(data.products, null, 2)}</pre> */}
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 }
 
-export const getServerSideProps = withPageAuthRequired();
+export const getServerSideProps = withPageAuthRequired({
+  getServerSideProps: async ctx => {
+    // return getServerPageGetProducts({}, ctx);
+
+    return {
+      props: {}
+    };
+  }
+});
+
+export default withApollo(ssrGetProducts.withPage()(Home));
